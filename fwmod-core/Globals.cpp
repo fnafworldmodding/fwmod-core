e9646199cd1a7109a412eac21042974003de9774
programminglaboratorys <programming.laboratorys@gmail.com>
2025-06-05 10:58:02 +0300

    chore: a global header to hold event manager and the Core logger

---
+++ fwmod-core/Globals.cpp
@@ -0,0 +1,4 @@
+#include "Globals.h"
+
+Logger CoreLogger;
+EventManager<Chunk*, BinaryReader&, __int64&> PluginsEventManager;
---
+++ fwmod-core/Globals.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef FWGLOBALS_H
+#define FWGLOBALS_H
+#pragma once
+
+#include "CCNParser\CCNPackage.h"
+#include "CCNParser\Chunks\Chunks.h"
+#include "EventManager.h"
+#include "Logger.h"
+#include <format>
+// common included headers
+
+#include "pch.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <Windows.h>
+#include <vector>
+#include <unordered_map>
+#include <mutex>
+#include <thread>
+#include <direct.h> // For _getcwd on Windows
+#include <tlhelp32.h>
+#include <fstream>
+#include <ctime>
+#include <functional>
+
+// globals variables
+extern Logger CoreLogger;
+extern EventManager<Chunk*, BinaryReader&, __int64&> PluginsEventManager;
+
+#endif // !FWGLOBALS_H
