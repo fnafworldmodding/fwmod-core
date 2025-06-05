919c444c69dae43dc2814ef6838232c9e111018d
programminglaboratorys <programming.laboratorys@gmail.com>
2025-06-05 11:03:45 +0300

    chore: remove no longer used functions since we are hooking

--- fwmod-core/GreenFreddyTools/CCNParser/CCNPackage.cpp
+++ fwmod-core/GreenFreddyTools/CCNParser/CCNPackage.cpp
@@ -1,18 +1,15 @@
 #include "CCNPackage.h"
-#include "../../Logger.h"
-
-static Logger CCNPackageLogger;
+#include "../../Globals.h"
 
 
 CCNPackage::CCNPackage() : runtimeVersion(0), runtimeSubversion(0), productVersion(0), productBuild(0) {
 	this->header = std::string();
 	this->unpackheader = std::string();
-	CCNPackageLogger.AddHandler(Logger::CreateCoreFileHandle("FWMCoreLogs.log"));
-	CCNPackageLogger.Debug("[CCNPackage] Initialized");
+	CoreLogger.Debug("[CCNPackage] Initialized");
 }
 
 CCNPackage::~CCNPackage() {
-	CCNPackageLogger.Debug("[CCNPackage] destructor called");
+	CoreLogger.Debug("[CCNPackage] destructor called");
 }
 
 void CCNPackage::ReadCCN(BinaryReader& buffer) {
@@ -21,15 +18,15 @@ void CCNPackage::ReadCCN(BinaryReader& buffer) {
 	// TODO: read pack files
 	// buffer.Skip(4); // already skipping uint when reading unpackheader
 	this->header = buffer.ReadFixedLengthString(4);
-	CCNPackageLogger.Info("Header: " + this->header);
+	CoreLogger.Info("Header: " + this->header);
 
 	this->runtimeVersion = buffer.ReadInt16();
 	this->runtimeSubversion = buffer.ReadInt16();
-	CCNPackageLogger.Info("Runtime Version: " + std::to_string(this->runtimeVersion) + "." + std::to_string(this->runtimeSubversion));
+	CoreLogger.Info("Runtime Version: " + std::to_string(this->runtimeVersion) + "." + std::to_string(this->runtimeSubversion));
 
 	this->productVersion = buffer.ReadInt32();
 	this->productBuild = buffer.ReadInt32();
-	CCNPackageLogger.Info("Proudct. Version: " + std::to_string(this->productVersion) + " Build: " + std::to_string(this->productBuild));
+	CoreLogger.Info("Proudct. Version: " + std::to_string(this->productVersion) + " Build: " + std::to_string(this->productBuild));
 }
 
 void CCNPackage::WriteCCN(BinaryWriter& buffer) {
--- fwmod-core/common.h
+++ fwmod-core/common.h
@@ -16,7 +16,7 @@
 #include <tlhelp32.h>
 #include <fstream>
 #include <ctime>
-#include <functional>  
+#include <functional>
 
 // (de)compressor for headers
 #include <cstdint>  
@@ -25,54 +25,5 @@
 
 
 
-// ctf runtime doesn't seem to ever have suspended threads, so this should be safe
-
-inline static void SuspendAllThreadsExceptThis() {
-    DWORD currentThreadId = GetCurrentThreadId();
-    DWORD currentProcessId = GetCurrentProcessId();
-
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (snapshot == INVALID_HANDLE_VALUE) return;
-
-    THREADENTRY32 threadEntry;
-    threadEntry.dwSize = sizeof(THREADENTRY32);
-
-    if (Thread32First(snapshot, &threadEntry)) {
-        do {
-            if (threadEntry.th32OwnerProcessID == currentProcessId &&
-                threadEntry.th32ThreadID != currentThreadId) {
-
-                HANDLE threadHandle = OpenThread(THREAD_SUSPEND_RESUME, FALSE, threadEntry.th32ThreadID);
-                if (threadHandle != NULL) {
-                    SuspendThread(threadHandle);
-                    CloseHandle(threadHandle);
-                }
-            }
-        } while (Thread32Next(snapshot, &threadEntry));
-    }
-
-    CloseHandle(snapshot);
-}
-
-inline static void UnsuspendAllThreads() {
-    DWORD currentProcessId = GetCurrentProcessId();
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
-    if (snapshot == INVALID_HANDLE_VALUE) return;
-    THREADENTRY32 threadEntry;
-    threadEntry.dwSize = sizeof(THREADENTRY32);
-
-    if (Thread32First(snapshot, &threadEntry)) {
-        do {
-            if (threadEntry.th32OwnerProcessID == currentProcessId) {
-                HANDLE threadHandle = OpenThread(THREAD_SUSPEND_RESUME, FALSE, threadEntry.th32ThreadID);
-                if (threadHandle != NULL) {
-                    ResumeThread(threadHandle);
-                    CloseHandle(threadHandle);
-                }
-            }
-        } while (Thread32Next(snapshot, &threadEntry));
-    }
-    CloseHandle(snapshot);
-}
 
 #endif // COMMON_H
