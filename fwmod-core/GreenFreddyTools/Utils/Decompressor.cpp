8a82548d7e8e46768fd42375000c1a9054b2a907
programminglaboratorys <programming.laboratorys@gmail.com>
2025-07-20 17:42:58 +0300

    feat: compression features & api

--- fwmod-core/GreenFreddyTools/Utils/Decompressor.cpp
+++ fwmod-core/GreenFreddyTools/Utils/Decompressor.cpp
@@ -101,7 +101,7 @@ std::vector<uint8_t> Decompressor::DecompressBlock(const std::vector<uint8_t>& d
         }
     }
 
-    if (result != Z_STREAM_END) {
+    if (result != Z_OK) {
         throw std::runtime_error(getErrorMessage(result));
     }
 
@@ -109,11 +109,51 @@ std::vector<uint8_t> Decompressor::DecompressBlock(const std::vector<uint8_t>& d
     return decompressedData;
 }
 
+uint8_t* Decompressor::DecompressBlockRaw(uint8_t* buffer, size_t bufferSize, size_t size) {
+    uLongf destLen = static_cast<uLong>(size);
+	uint8_t* data = new uint8_t[size]; // decompressed data pointer
+    int result;
+
+    if (IsZlib(buffer, bufferSize)) {
+        // ZEXTERN int ZEXPORT uncompress(Bytef * dest, uLongf * destLen,
+        //    const Bytef * source, uLong sourceLen);
+        result = uncompress(data, &destLen, buffer, static_cast<uLong>(bufferSize));
+    }
+    else {
+        // Raw deflate stream (no zlib header)
+        z_stream strm = {};
+        strm.next_in = const_cast<Bytef*>(buffer);
+        strm.avail_in = static_cast<uInt>(bufferSize);
+        strm.next_out = data;
+        strm.avail_out = static_cast<uInt>(size);
+
+        result = inflateInit2(&strm, -MAX_WBITS); // raw deflate
+        if (result == Z_OK) {
+            result = inflate(&strm, Z_FINISH);
+            inflateEnd(&strm);
+            destLen = strm.total_out;
+        }
+    }
+
+    if (result != Z_OK) {
+        throw std::runtime_error(getErrorMessage(result));
+    }
+
+    return data;
+}
+
 
 
 
 bool Decompressor::IsZlib(const std::vector<uint8_t>& check) {
     if (check.size() < 2) return false;
+    if (check[0] != 0x78) return false;
+    return check[1] == 0x01 || check[1] == 0x5E || check[1] == 0x9C || check[1] == 0xDA;
+}
+
+
+bool Decompressor::IsZlib(uint8_t*& check, size_t size) {
+    if (size < 2) return false;
     if (check[0] != 0x78) return false;
     return check[1] == 0x01 || check[1] == 0x5E || check[1] == 0x9C || check[1] == 0xDA;
 }
@@ -141,4 +181,130 @@ std::string Decompressor::getErrorMessage(int errorCode) {
     default:
         return "Unknown error";
     }
+}
+
+std::vector<uint8_t> Decompressor::Compress(const std::vector<uint8_t>& data, int& compressed, int& result) {
+    if (data.empty()) {
+        compressed = 0;
+        return {};
+    }
+
+    uLongf compBound = compressBound(static_cast<uLong>(data.size()));
+    std::vector<uint8_t> compressedData(compBound);
+
+    result = compress2(
+        compressedData.data(),
+        &compBound,
+        data.data(),
+        static_cast<uLong>(data.size()),
+        Z_BEST_SPEED
+    );
+
+    if (result != Z_OK) {
+        throw std::runtime_error(Decompressor::getErrorMessage(result));
+    }
+
+    compressedData.resize(compBound);
+    compressed = static_cast<int>(compBound);
+    return compressedData;
+}
+
+
+uint8_t* Decompressor::CompressRetRaw(const std::vector<uint8_t>& data, int& compressed, int& result) {
+    if (data.empty()) {
+        compressed = 0;
+        return {};
+    }
+
+    uLongf compBound = compressBound(static_cast<uLong>(data.size()));
+	uint8_t* compressedDataPtr = new uint8_t[compBound];
+
+    result = compress2(
+        compressedDataPtr,
+        &compBound,
+        data.data(),
+        static_cast<uLong>(data.size()),
+        Z_BEST_SPEED
+    );
+
+    if (result != Z_OK) {
+        throw std::runtime_error(Decompressor::getErrorMessage(result));
+    }
+
+    compressed = static_cast<int>(compBound);
+    return compressedDataPtr;
+}
+
+uint8_t* Decompressor::CompressRaw(uint8_t* data, size_t size, size_t& compressed, int& result) {
+    uLongf compBound = compressBound(static_cast<uLong>(size));
+    uint8_t* compressedDataPtr = new uint8_t[compBound];
+
+    result = compress2(
+        compressedDataPtr,
+        &compBound,
+        data,
+        static_cast<uLong>(size),
+        Z_BEST_SPEED
+    );
+
+    if (result != Z_OK) {
+		delete[] compressedDataPtr;
+        throw std::runtime_error(Decompressor::getErrorMessage(result));
+    }
+
+    compressed = static_cast<int>(compBound);
+    return compressedDataPtr;
+}
+
+uint8_t* Decompressor::CompressZlibRaw(uint8_t* data, size_t size, size_t& compressed, int& result) {
+    // Initialize the zlib stream
+    z_stream strm{};
+    strm.avail_in = static_cast<uInt>(size);
+    strm.next_in = data;
+
+    uLongf compBound = compressBound(static_cast<uLong>(size));
+    uint8_t* compressedDataPtr = new uint8_t[compBound];
+
+
+    result = deflateInit2(&strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED, MAX_WBITS, 8, Z_DEFAULT_STRATEGY);
+    if (result != Z_OK) {
+        delete[] compressedDataPtr;
+        throw std::runtime_error(Decompressor::getErrorMessage(result));
+    }
+
+    strm.avail_out = compBound;
+    strm.next_out = compressedDataPtr;
+
+    result = deflate(&strm, Z_FINISH);
+    if (result < Z_OK) {
+        deflateEnd(&strm);
+        delete[] compressedDataPtr;
+        throw std::runtime_error(Decompressor::getErrorMessage(result));
+    }
+
+    // Get the size of the compressed data
+    compressed = compBound - strm.avail_out;
+
+    // Clean up
+    deflateEnd(&strm);
+    return compressedDataPtr;
+}
+
+
+uint8_t* Decompressor::CompressZlibRaw2(uint8_t* data, size_t size, size_t& compressed, int& result) {
+    // Calculate the maximum compressed size
+    uLongf compBound = compressBound(static_cast<uLong>(size));
+    uint8_t* compressedDataPtr = new uint8_t[compBound];
+
+    // Perform the compression
+    result = compress(compressedDataPtr, &compBound, data, static_cast<uLong>(size));
+
+    if (result != Z_OK) {
+        delete[] compressedDataPtr;
+        throw std::runtime_error(Decompressor::getErrorMessage(result));
+    }
+
+    // Set the actual size of the compressed data
+    compressed = static_cast<size_t>(compBound);
+    return compressedDataPtr;
 }
--- fwmod-core/GreenFreddyTools/Utils/Decompressor.h
+++ fwmod-core/GreenFreddyTools/Utils/Decompressor.h
@@ -12,13 +12,25 @@
 
 class DLLCALL Decompressor {
 public:
+	// is there a reason to have a result parameter when I throw an error? lol?
+    // TODO: make types consistent
+    static uint8_t*             CompressZlibRaw(uint8_t* data, size_t size, size_t& compressed, int& result);
+	static uint8_t*             CompressZlibRaw2(uint8_t* data, size_t size, size_t& compressed, int& result);
+    static uint8_t*             CompressRaw(uint8_t* data, size_t size, size_t& compressed, int& result);
+    static uint8_t*             CompressRetRaw(const std::vector<uint8_t>& data, int& compressed, int& result);
+    static std::vector<uint8_t> Compress(const std::vector<uint8_t>& data, int& compressed, int& result);
+    //
+    static uint8_t*             DecompressRaw(const uint8_t* buffer, size_t bufferSize, int& decompressed);
     static std::vector<uint8_t> Decompress(BinaryReader& buffer, int& decompressed);
     static int                  DecompressChunk(Chunk& chunk, int& decompressed);
     static int                  DecompressChunk(Chunk& chunk, BinaryReader& buffer, int& decompressed);
     static std::vector<uint8_t> DecompressBlock(const std::vector<uint8_t>& data, int size);
+    static uint8_t*             DecompressBlockRaw(uint8_t* buffer, size_t bufferSize, size_t size);
     static int                  DecompressBlock(Chunk& chunk, std::vector<uint8_t>& compressedData, int size);
+    //
     static std::string          getErrorMessage(int errorCode);
     static bool                 IsZlib(const std::vector<uint8_t>& check);
+    static bool                 IsZlib(uint8_t*& check, size_t size);
 };
 
 #endif // DECOMPRESSOR_H
